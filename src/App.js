e673d7697a48f5a00fa4ac8a563284898851f7ef
Khoi Nguyen <khoi4real@Khois-MacBook-Pro.local>
2021-09-17 23:22:28 -0400

    deploy my react app to github pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './pages';
 import SignInPage from './pages/signin';
 
